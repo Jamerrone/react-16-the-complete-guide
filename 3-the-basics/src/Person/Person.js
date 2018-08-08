6bc9880bb17a8b0008df2a58c1bb41773eab235b
James Peter Perrone Jefferies <james.p.perrone@gmail.com>
2018-08-08 17:18:24 +0200

    Update: outputting dynamic content

--- 3-the-basics/src/Person/Person.js
+++ 3-the-basics/src/Person/Person.js
@@ -1,7 +1,9 @@
 import React from "react";
 
 const Person = () => {
-  return <p>I'm a Person!</p>;
+  return (
+    <p>I'm a Person and I am {Math.floor(Math.random() * 30 + 1)} years old!</p>
+  );
 };
 
 export default Person;
