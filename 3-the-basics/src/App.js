701df0fa125338887c04b6b6eac307e183de8460
James Peter Perrone Jefferies <james.p.perrone@gmail.com>
2018-08-08 17:49:27 +0200

    Update: working with states and event handlers

--- 3-the-basics/src/App.js
+++ 3-the-basics/src/App.js
@@ -3,16 +3,44 @@ import Person from "./Person/Person";
 import "./App.css";
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: "James", age: "25" },
+      { name: "Jessica", age: "22" },
+      { name: "Serge", age: "47" }
+    ]
+  };
+
+  switchNameHandler = () => {
+    this.setState({
+      persons: [
+        { name: "James Peter", age: "25" },
+        { name: "Jessica Perrone", age: "22" },
+        { name: "Serge Muelink", age: "47" }
+      ]
+    });
+  };
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <Person name="James" age="25" />
-        <Person name="Jessica" age="22">
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        >
           My Hobbies: Racing
         </Person>
-        <Person name="Serge" age="47" />
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        />
       </div>
     );
   }
