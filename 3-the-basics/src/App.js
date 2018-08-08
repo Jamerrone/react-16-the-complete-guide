981c1219c461fd9f629aaf2dd5b1a19588b77574
James Peter Perrone Jefferies <james.p.perrone@gmail.com>
2018-08-08 17:28:48 +0200

    Update: working with props and children

--- 3-the-basics/src/App.js
+++ 3-the-basics/src/App.js
@@ -8,9 +8,11 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <Person />
-        <Person />
-        <Person />
+        <Person name="James" age="25" />
+        <Person name="Jessica" age="22">
+          My Hobbies: Racing
+        </Person>
+        <Person name="Serge" age="47" />
       </div>
     );
   }
