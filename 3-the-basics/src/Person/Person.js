1d64f180389d153d57d52835737d1161916d9371
James Peter Perrone Jefferies <james.p.perrone@gmail.com>
2018-08-09 16:16:13 +0200

    Update: adding styling with stylesheets

--- 3-the-basics/src/Person/Person.js
+++ 3-the-basics/src/Person/Person.js
@@ -1,8 +1,9 @@
 import React from "react";
+import "./Person.css";
 
 const Person = props => {
   return (
-    <div>
+    <div className="Person">
       <p onClick={props.onClick}>
         I'm a {props.name} and I am {props.age} years old!
       </p>
