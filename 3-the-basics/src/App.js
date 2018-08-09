de802934e7f066b1c837fe5deb28530a1ab3c78c
James Peter Perrone Jefferies <james.p.perrone@gmail.com>
2018-08-09 16:30:09 +0200

    The basics (#3)
    * Create: the necessary files for a react project
    
    * Clean: project folder structure
    
    * Create: our first functional component
    
    * Update: outputting dynamic content
    
    * Update: working with props and children
    
    * Update: working with states and event handlers
    
    * Update: passing method references between components
    
    * Update: adding two way binding
    
    * Update: adding styling with stylesheets
    
    * Update: working with inline styles
    
    * Remove: readme.md file

---
+++ 3-the-basics/src/App.js
@@ -0,0 +1,75 @@
+import React, { Component } from "react";
+import Person from "./Person/Person";
+import "./App.css";
+
+class App extends Component {
+  state = {
+    persons: [
+      { name: "James", age: "25" },
+      { name: "Jessica", age: "22" },
+      { name: "Serge", age: "47" }
+    ]
+  };
+
+  switchNameHandler = newName => {
+    this.setState({
+      persons: [
+        { name: newName, age: "25" },
+        { name: "Jessica Perrone", age: "22" },
+        { name: "Serge Muelink", age: "47" }
+      ]
+    });
+  };
+
+  nameChangedHandler = event => {
+    this.setState({
+      persons: [
+        { name: event.target.value, age: "25" },
+        { name: "Jessica", age: "22" },
+        { name: "Serge", age: "47" }
+      ]
+    });
+  };
+
+  render() {
+    const style = {
+      backgroundColor: "white",
+      border: "1px solid blue",
+      cursor: "pointer",
+      font: "inherit",
+      padding: ".5em"
+    };
+
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working!</p>
+        {/* The bind method on line 40 is the preferred method. */}
+        <button
+          style={style}
+          onClick={() => this.switchNameHandler("James Peter")}
+        >
+          Switch Name
+        </button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+          onChange={this.nameChangedHandler}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          onClick={this.switchNameHandler.bind(this, "James Perrone")}
+        >
+          My Hobbies: Racing
+        </Person>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
