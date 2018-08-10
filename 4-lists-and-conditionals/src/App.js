ffd20c21ecf0cdb03ba394c814c5c7a3e0225242
James Peter Perrone Jefferies <james.p.perrone@gmail.com>
2018-08-10 20:24:15 +0200

    Update: rendering content conditionally 'the right way'

--- 4-lists-and-conditionals/src/App.js
+++ 4-lists-and-conditionals/src/App.js
@@ -46,6 +46,31 @@ class App extends Component {
       padding: ".5em"
     };
 
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          <Person
+            name={this.state.persons[0].name}
+            age={this.state.persons[0].age}
+            onChange={this.nameChangedHandler}
+          />
+          <Person
+            name={this.state.persons[1].name}
+            age={this.state.persons[1].age}
+            onClick={this.switchNameHandler.bind(this, "James Perrone")}
+          >
+            My Hobbies: Racing
+          </Person>
+          <Person
+            name={this.state.persons[2].name}
+            age={this.state.persons[2].age}
+          />
+        </div>
+      );
+    }
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
@@ -54,26 +79,7 @@ class App extends Component {
         <button style={style} onClick={this.togglePersonsHandler}>
           Toggle Persons
         </button>
-        {this.state.showPersons ? (
-          <div>
-            <Person
-              name={this.state.persons[0].name}
-              age={this.state.persons[0].age}
-              onChange={this.nameChangedHandler}
-            />
-            <Person
-              name={this.state.persons[1].name}
-              age={this.state.persons[1].age}
-              onClick={this.switchNameHandler.bind(this, "James Perrone")}
-            >
-              My Hobbies: Racing
-            </Person>
-            <Person
-              name={this.state.persons[2].name}
-              age={this.state.persons[2].age}
-            />
-          </div>
-        ) : null}
+        {persons}
       </div>
     );
   }
