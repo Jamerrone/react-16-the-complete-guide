d30a28b98778696356094c7bb6234db7cacff941
James Peter Perrone Jefferies <james.p.perrone@gmail.com>
2018-08-13 16:46:45 +0200

    Update: a better project structure

--- 7-diving-deeper-into-components/src/containers/App.js
+++ 7-diving-deeper-into-components/src/containers/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Person from "./Person/Person";
+import Person from "../components/Persons/Person/Person";
 import classes from "./App.css";
 
 class App extends Component {
