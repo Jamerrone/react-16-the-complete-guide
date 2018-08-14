e06bd1d1da26951584246d84dcae55f9c5eae053
James Peter Perrone Jefferies <james.p.perrone@gmail.com>
2018-08-14 14:49:07 +0200

    Update: converting stateless to steful components

--- 7-diving-deeper-into-components/src/components/Persons/Person/Person.js
+++ 7-diving-deeper-into-components/src/components/Persons/Person/Person.js
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { Component } from "react";
 import classes from "./Person.css";
 
-const Person = props => {
-  return (
-    <div className={classes.Person}>
-      <p onClick={props.onClick}>
-        I'm a {props.name} and I am {props.age} years old!
-      </p>
-      <p>{props.children}</p>
-      <input type="text" value={props.name} onChange={props.onChange} />
-    </div>
-  );
-};
+class Person extends Component {
+  render() {
+    return (
+      <div className={classes.Person}>
+        <p onClick={this.props.onClick}>
+          I'm a {this.props.name} and I am {this.props.age} years old!
+        </p>
+        <p>{this.props.children}</p>
+        <input
+          type="text"
+          value={this.props.name}
+          onChange={this.props.onChange}
+        />
+      </div>
+    );
+  }
+}
 
 export default Person;
--- 7-diving-deeper-into-components/src/components/Persons/Persons.js
+++ 7-diving-deeper-into-components/src/components/Persons/Persons.js
@@ -1,17 +1,20 @@
-import React from "react";
+import React, { Component } from "react";
 import Person from "./Person/Person";
 
-const Persons = props =>
-  props.persons.map((person, index) => {
-    return (
-      <Person
-        name={person.name}
-        age={person.age}
-        onClick={() => props.onClick(index)}
-        key={person.id}
-        onChange={event => props.onChange(event, person.id)}
-      />
-    );
-  });
+class Persons extends Component {
+  render() {
+    return this.props.persons.map((person, index) => {
+      return (
+        <Person
+          name={person.name}
+          age={person.age}
+          onClick={() => this.props.onClick(index)}
+          key={person.id}
+          onChange={event => this.props.onChange(event, person.id)}
+        />
+      );
+    });
+  }
+}
 
 export default Persons;
