ea9766208b7c258cd42c41571757254a3fd5e5d0
James Peter Perrone Jefferies <james.p.perrone@gmail.com>
2018-08-15 14:51:44 +0200

    Update: passing unknown props

--- 7-diving-deeper-into-components/src/hoc/WithClass.js
+++ 7-diving-deeper-into-components/src/hoc/WithClass.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { Component } from "react";
+
+// const WithClass = (WrappedComponent, className) => {
+//   return props => (
+//     <div className={className}>
+//       <WrappedComponent {...props} />
+//     </div>
+//   );
+// };
 
 const WithClass = (WrappedComponent, className) => {
-  return props => (
-    <div className={className}>
-      <WrappedComponent />
-    </div>
-  );
+  return class extends Component {
+    render() {
+      return (
+        <div className={className}>
+          <WrappedComponent {...this.props} />
+        </div>
+      );
+    }
+  };
 };
 
 export default WithClass;
