7cc53e0cd97d217e329073bf1bf038e1f424030a
James Peter Perrone Jefferies <james.p.perrone@gmail.com>
2018-08-18 16:02:08 +0200

    Update: connecting state to build controls

--- 8-the-burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ 8-the-burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,13 @@ import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
+const INGREDIENT_PRICES = {
+  bacon: 0.7,
+  salad: 0.5,
+  cheese: 0.4,
+  meat: 1.3
+};
+
 class BurgerBuilder extends Component {
   state = {
     ingredients: {
@@ -11,14 +18,33 @@ class BurgerBuilder extends Component {
       salad: 0,
       cheese: 0,
       meat: 0
-    }
+    },
+    totalPrice: 4
   };
+
+  addIngredientHandler = type => {
+    const oldCount = this.state.ingredients[type];
+    const updatedCount = oldCount + 1;
+    const updatedIngredients = { ...this.state.ingredients };
+    const oldPrice = this.state.totalPrice;
+    const priceAddition = INGREDIENT_PRICES[type];
+    const updatedPrice = oldPrice + priceAddition;
+
+    updatedIngredients[type] = updatedCount;
+
+    this.setState({
+      ingredients: updatedIngredients,
+      totalPrice: updatedPrice
+    });
+  };
+
+  removeIngredientHandler = type => {};
 
   render() {
     return (
       <Aux>
         <Burger ingredients={this.state.ingredients} />
-        <BuildControls />
+        <BuildControls ingredientAdded={this.addIngredientHandler} />
       </Aux>
     );
   }
