82584a392b5dc4dda8dc700698503f0dd474c411
James Peter Perrone Jefferies <james.p.perrone@gmail.com>
2018-08-19 17:06:33 +0200

    Update: adding the price to the order summary

--- 8-the-burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ 8-the-burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -105,6 +105,7 @@ class BurgerBuilder extends Component {
           modalClosed={this.purchaseCancelHandler}
         >
           <OrderSummary
+            price={this.state.totalPrice}
             ingredients={this.state.ingredients}
             purchaseCancelled={this.purchaseCancelHandler}
             purchaseContinued={this.purchaseContinueHandler}
