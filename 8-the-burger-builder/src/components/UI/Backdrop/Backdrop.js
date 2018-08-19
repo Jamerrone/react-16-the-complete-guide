b70e2c2dac1840ad03db55015f0f93bcee3ff179
James Peter Perrone Jefferies <james.p.perrone@gmail.com>
2018-08-19 16:46:27 +0200

    Update: implementing the backdrop component

---
+++ 8-the-burger-builder/src/components/UI/Backdrop/Backdrop.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import classes from './Backdrop.css';
+
+const backdrop = props => {
+  return props.show ? (
+    <div className={classes.Backdrop} onClick={props.clicked} />
+  ) : null;
+};
+
+export default backdrop;
