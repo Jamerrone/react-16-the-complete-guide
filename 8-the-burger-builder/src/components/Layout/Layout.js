5a30f4aa7828e595951eaea509c0f47e8a468199
James Peter Perrone Jefferies <james.p.perrone@gmail.com>
2018-08-21 17:03:28 +0200

    Update: navigation, sidedrawer & responsive adjustments

--- 8-the-burger-builder/src/components/Layout/Layout.js
+++ 8-the-burger-builder/src/components/Layout/Layout.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import Aux from '../../hoc/Aux';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 import classes from './Layout.css';
 
 const layout = props => (
   <Aux>
     <Toolbar />
+    <SideDrawer />
     <main className={classes.Content}>{props.children}</main>
   </Aux>
 );
--- 8-the-burger-builder/src/components/Navigation/Toolbar/Toolbar.js
+++ 8-the-burger-builder/src/components/Navigation/Toolbar/Toolbar.js
@@ -1,14 +1,19 @@
 import React from 'react';
 
 import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
 
 import classes from './Toolbar.css';
 
 const toolbar = props => (
   <header className={classes.Toolbar}>
     <div>MENU</div>
-    <Logo />
-    <nav>...</nav>
+    <div className={classes.Logo}>
+      <Logo />
+    </div>
+    <nav className={classes.DesktopOnly}>
+      <NavigationItems />
+    </nav>
   </header>
 );
 
