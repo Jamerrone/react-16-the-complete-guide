739953701bfb71d0459f6d6385747345121ad2e8
James Peter Perrone Jefferies <james.p.perrone@gmail.com>
2018-08-22 20:25:24 +0200

    A Real App: The Burger Builder (Basic Version) (#8)
    * Create: a new project using 'create-react-app'
    
    * Update: project setup and cleanup
    
    * Create: the layout component
    
    * Create: the burger builder container
    
    * Create: a dynamic ingredient component
    
    * Add: prop type validation
    
    * Create: the burger component
    
    * Update: outputting burger ingredients dynamically
    
    * Update: calc the ingredient sum dynamically
    
    * Create: the build control component
    
    * Update: outputting multiple build controls
    
    * Update: connecting state to build controls
    
    * Update: removing ingredients safely
    
    * Update: displaying and updating the burger price
    
    * Update: adding the order button
    
    * Update: creating the order summary modal
    
    * Update: showing and hiding the modal
    
    * Update: implementing the backdrop component
    
    * Update: adding a custom button component
    
    * Update: implementing the button component
    
    * Update: adding the price to the order summary
    
    * Update: adding a toolbar
    
    * Update: using a logo in our application
    
    * Update: navigation, sidedrawer & responsive adjustments
    
    * Update: reusing the backdrop
    
    * Update: adding a sidedrawer toggle button
    
    * Update: adding a hamburger icon
    
    * Update: improving performance
    
    * Update: changing the folder structure

---
+++ 8-the-burger-builder/src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import BuildControl from './BuildControl/BuildControl';
+
+import classes from './BuildControls.css';
+
+const controls = [
+  { label: 'Bacon', type: 'bacon' },
+  { label: 'Salad', type: 'salad' },
+  { label: 'Cheese', type: 'cheese' },
+  { label: 'Meat', type: 'meat' }
+];
+
+const buildControls = props => (
+  <div className={classes.BuildControls}>
+    <p>
+      Current Price: <strong>${props.price.toFixed(2)}</strong>
+    </p>
+    {controls.map(ctrl => (
+      <BuildControl
+        added={() => props.ingredientAdded(ctrl.type)}
+        disabled={props.disabled[ctrl.type]}
+        key={ctrl.label}
+        label={ctrl.label}
+        removed={() => props.ingredientRemoved(ctrl.type)}
+      />
+    ))}
+    <button
+      className={classes.OrderButton}
+      disabled={!props.purchasable}
+      onClick={props.ordered}
+    >
+      ORDER NOW
+    </button>
+  </div>
+);
+
+export default buildControls;
---
+++ 8-the-burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,128 @@
+import React, { Component } from 'react';
+
+import Aux from '../../hoc/Aux/Aux';
+import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+import Modal from '../../components/UI/Modal/Modal';
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+
+const INGREDIENT_PRICES = {
+  bacon: 0.7,
+  salad: 0.5,
+  cheese: 0.4,
+  meat: 1.3
+};
+
+class BurgerBuilder extends Component {
+  state = {
+    ingredients: {
+      bacon: 0,
+      salad: 0,
+      cheese: 0,
+      meat: 0
+    },
+    totalPrice: 4,
+    purchasable: false,
+    purchasing: false
+  };
+
+  updatePurchaseState(ingredients) {
+    const sum = Object.keys(ingredients)
+      .map(igKey => {
+        return ingredients[igKey];
+      })
+      .reduce((sum, el) => {
+        return sum + el;
+      }, 0);
+
+    this.setState({ purchasable: sum > 0 });
+  }
+
+  addIngredientHandler = type => {
+    const oldCount = this.state.ingredients[type];
+    const updatedCount = oldCount + 1;
+    const updatedIngredients = { ...this.state.ingredients };
+    const oldPrice = this.state.totalPrice;
+    const priceAddition = INGREDIENT_PRICES[type];
+    const updatedPrice = oldPrice + priceAddition;
+
+    updatedIngredients[type] = updatedCount;
+
+    this.setState({
+      ingredients: updatedIngredients,
+      totalPrice: updatedPrice
+    });
+
+    this.updatePurchaseState(updatedIngredients);
+  };
+
+  removeIngredientHandler = type => {
+    const oldCount = this.state.ingredients[type];
+    if (oldCount <= 0) {
+      return;
+    }
+    const updatedCount = oldCount - 1;
+    const updatedIngredients = { ...this.state.ingredients };
+    const oldPrice = this.state.totalPrice;
+    const priceDeduction = INGREDIENT_PRICES[type];
+    const updatedPrice = oldPrice - priceDeduction;
+
+    updatedIngredients[type] = updatedCount;
+
+    this.setState({
+      ingredients: updatedIngredients,
+      totalPrice: updatedPrice
+    });
+
+    this.updatePurchaseState(updatedIngredients);
+  };
+
+  purchaseHandler = () => {
+    this.setState({ purchasing: true });
+  };
+
+  purchaseCancelHandler = () => {
+    this.setState({ purchasing: false });
+  };
+
+  purchaseContinueHandler = () => {
+    alert('You continued!');
+  };
+
+  render() {
+    const disabledInfo = {
+      ...this.state.ingredients
+    };
+
+    for (let key in disabledInfo) {
+      disabledInfo[key] = disabledInfo[key] <= 0;
+    }
+
+    return (
+      <Aux>
+        <Modal
+          show={this.state.purchasing}
+          modalClosed={this.purchaseCancelHandler}
+        >
+          <OrderSummary
+            price={this.state.totalPrice}
+            ingredients={this.state.ingredients}
+            purchaseCancelled={this.purchaseCancelHandler}
+            purchaseContinued={this.purchaseContinueHandler}
+          />
+        </Modal>
+        <Burger ingredients={this.state.ingredients} />
+        <BuildControls
+          disabled={disabledInfo}
+          ingredientAdded={this.addIngredientHandler}
+          ingredientRemoved={this.removeIngredientHandler}
+          ordered={this.purchaseHandler}
+          price={this.state.totalPrice}
+          purchasable={this.state.purchasable}
+        />
+      </Aux>
+    );
+  }
+}
+
+export default BurgerBuilder;
