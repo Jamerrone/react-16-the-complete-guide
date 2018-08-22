906de1c0eba661b67ba1875c44ada6ce9e1ff524
James Peter Perrone Jefferies <james.p.perrone@gmail.com>
2018-08-22 18:06:15 +0200

    Update: adding a sidedrawer toggle button

--- 8-the-burger-builder/src/components/Navigation/Toolbar/Toolbar.js
+++ 8-the-burger-builder/src/components/Navigation/Toolbar/Toolbar.js
@@ -2,12 +2,13 @@ import React from 'react';
 
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 
 import classes from './Toolbar.css';
 
 const toolbar = props => (
   <header className={classes.Toolbar}>
-    <div>MENU</div>
+    <DrawerToggle clicked={props.drawerToggleClicked} />
     <div className={classes.Logo}>
       <Logo />
     </div>
